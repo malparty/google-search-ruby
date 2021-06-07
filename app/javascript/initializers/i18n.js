f5db9024f7e1214b3de3a4acc6b4d9221eadd5ca
Xavier M <xavier@nimblehq.co>
2021-06-07 14:56:51 +0700

    [#21] Reset repository to start again with PR convention

--- app/javascript/initializers/i18n.js
+++
@@ -1,1 +0,0 @@
-I18n.locale = document.getElementsByTagName('html')[0].getAttribute('lang') || I18n.defaultLocale;
--- bin/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Exit on fail
-set -e
-
-bundle check || bundle install
-
-bundle exec rspec $@ # adds any rspec args passed down from CLI

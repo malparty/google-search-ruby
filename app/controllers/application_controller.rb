a57d6dae08e68608269a75a3dd853dc3a7a541cb
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-09 11:32:52 +0700

    [#5] Add lastname and firstname to registration views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@
 
 class ApplicationController < ActionController::Base
   include Localization
+
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:firstname, :lastname, :email, :password, :current_password)
+    end
+  end
 end

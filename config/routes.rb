38e6db8c9a7774057048f2a2bf996c5d165f4ffe
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-09 18:21:41 +0700

    [#293] Setup DoorKeeper to embbed OAuth2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'keywords#index'
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
 
   devise_for :users
 
+  root to: 'keywords#index'
+
   resources :keywords, only: :index
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

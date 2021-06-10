e75afba5c7bb89d6f424fc0974015ec1b355dd8c
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-10 15:01:40 +0700

    [#17] Update doorkeeper scope to match API urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   root to: 'keywords#index'
 
-  use_doorkeeper do
-    skip_controllers :authorizations, :applications, :authorized_applications
-  end
-
   devise_for :users
 
   resources :keywords, only: :index
 
   namespace :api do
     namespace :v1 do
+      # User sign_up
       resources :users, only: %i[create]
+    end
+  end
+  scope 'api/v1' do
+    # OAuth2 (token, revoke, ...)
+    use_doorkeeper do
+      skip_controllers :authorizations, :applications, :authorized_applications
     end
   end
 end

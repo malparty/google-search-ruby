1773c0d2abab6cc6ebe32518dce438b9b2746bab
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-11 16:27:15 +0700

    [#17] Add basic request tests for OAuth token revoke requests Rebase from develop rails_helper rspec

--- spec/requests/oauth2_spec.rb
+++ spec/requests/oauth2_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 describe Doorkeeper::TokensController, type: :request do
-
   context 'when user ask for a token with good request' do
     it 'return a refreshable token' do
       post 'create', params: token_request_params
@@ -30,6 +29,27 @@ describe Doorkeeper::TokensController, type: :request do
       post 'create', params: params
 
       expect(JSON.parse(response.body)['error']).to eq('invalid_grant')
+    end
+  end
+
+  context 'when user refresh an existing token on time' do
+    it 'return the refreshed token' do
+      token = query_token
+      refreshed_token = query_refresh_token token['refresh_token']
+
+      expect(refreshed_token['token_type']).to eq('Bearer')
+    end
+  end
+
+  context 'when user revoke an existing token' do
+    it 'return no error message ' do
+      token = query_token
+      params = token_request_params
+      params[:token] = token
+
+      post 'revoke', params: params
+
+      expect(JSON.parse(response.body)).to eq({})
     end
   end
 end

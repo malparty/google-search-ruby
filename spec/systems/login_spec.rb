1e02d4355220f12a142e3fffb36f86314ae00e79
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-11 13:18:16 +0700

    [#5] Add login and signup system tests

--- spec/systems/login_spec.rb
+++ spec/systems/login_spec.rb
@@ -3,28 +3,30 @@
 require 'rails_helper'
 
 describe 'login', type: :system do
-  context 'when unlogged user reach the app' do
-    it 'should redirect him to the login page' do
+  context 'when non logged user reach the app' do
+    it 'redirect the user to the login page' do
       visit root_path
-      expect(find('h2')).to have_content('Log in')
-      expect(find('form')).to have_field('user_email')
-      expect(find('form')).to have_field('user_password')
+
+      expect(page).to have_current_path(new_user_session_path)
     end
   end
 
   context 'when valid credentials' do
-    it 'should log the user in and display a flash message' do
+    it 'log the user in and display a flash message' do
       sign_in
+
       expect(page).to have_content(I18n.t('devise.sessions.signed_in'))
     end
   end
 
   context 'when invalid credentials' do
-    it 'should refuse login and display an error' do
+    it 'refuse login and display an error' do
       bad_user = Fabricate(:user)
       bad_user.password = 'bad'
-      sign_in bad_user
       error_msg = I18n.t('devise.failure.invalid').to_s.gsub('%{authentication_keys}', 'Email')
+
+      sign_in bad_user
+
       expect(page).to have_content(error_msg)
     end
   end

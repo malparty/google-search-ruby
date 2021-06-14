c713787db2e3bf58e6d93b29261b2a0dac801347
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-14 11:57:42 +0700

    [#5] Update system tests to use faster sign_in method helper

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -1,24 +1,37 @@
 # frozen_string_literal: true
 
-module AuthenticationHelper
-  def sign_in(user = nil)
-    user ||= Fabricate(:user)
-
-    visit root_path
-
-    fill_in 'user_email',    with: user.email
-    fill_in 'user_password', with: user.password
-    click_button 'Log in'
+module DeviseHelpers
+  module RequestHelpers
+    # def sign_in_user(user)
+    #   request.env['devise.mapping'] = Devise.mappings[:user]
+    #   sign_in(user)
+    # end
   end
 
-  def sign_up(email, password, password_confirm = nil)
-    user = Fabricate(:user)
-    visit new_user_registration_path
-    fill_in 'user_firstname', with: user.firstname
-    fill_in 'user_lastname', with: user.lastname
-    fill_in 'user_email', with: email
-    fill_in 'user_password', with: password
-    fill_in 'user_password_confirmation', with: password_confirm || password
-    click_button 'Sign up'
+  module SystemHelpers
+    def sign_in_ui(user = nil)
+      user ||= Fabricate(:user)
+
+      visit root_path
+
+      fill_in 'user_email',    with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'Log in'
+    end
+
+    def sign_in(user)
+      login_as user, scope: :user
+    end
+
+    def sign_up_ui(email, password, password_confirm = nil)
+      user = Fabricate(:user)
+      visit new_user_registration_path
+      fill_in 'user_firstname', with: user.firstname
+      fill_in 'user_lastname', with: user.lastname
+      fill_in 'user_email', with: email
+      fill_in 'user_password', with: password
+      fill_in 'user_password_confirmation', with: password_confirm || password
+      click_button 'Sign up'
+    end
   end
 end
--- spec/systems/signup_spec.rb
+++ spec/systems/signup_spec.rb
@@ -22,7 +22,7 @@ describe 'signup', type: :system do
 
   context 'when non logged user sign up with good data' do
     it 'create an account and log the user in' do
-      sign_up 'good@email.com', 'password123'
+      sign_up_ui 'good@email.com', 'password123'
 
       expect(page).to have_content(I18n.t('devise.registrations.signed_up'))
     end
@@ -30,7 +30,7 @@ describe 'signup', type: :system do
 
   context 'when an non logged user sign up with bad email' do
     it 'stays on the same page' do
-      sign_up 'bad_email', 'password123'
+      sign_up_ui 'bad_email', 'password123'
 
       expect(page).to have_current_path new_user_registration_path
     end
@@ -38,7 +38,7 @@ describe 'signup', type: :system do
 
   context 'when an non logged user sign up with too short password' do
     it 'display an error message' do
-      sign_up 'good@email.com', '123'
+      sign_up_ui 'good@email.com', '123'
 
       expect(page).to have_selector('#error_explanation li')
     end
@@ -46,7 +46,7 @@ describe 'signup', type: :system do
 
   context 'when an non logged user sign up with bad password confirmation' do
     it 'display an error message' do
-      sign_up 'good@email.com', 'complex123password', 'differentPassword123'
+      sign_up_ui 'good@email.com', 'complex123password', 'differentPassword123'
 
       expect(page).to have_selector('#error_explanation li')
     end
@@ -54,7 +54,7 @@ describe 'signup', type: :system do
 
   context 'when a logged user reach the sign up page' do
     it 'redirect him to the root_page' do
-      sign_in
+      sign_in_ui
       visit new_user_registration_path
 
       expect(page).to have_current_path(root_path)

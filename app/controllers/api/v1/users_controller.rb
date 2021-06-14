ce5de142897ddab0d29a45c4bbbced0495bd7363
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-14 09:27:04 +0700

    [#17] Remove logic from controller to be in user model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -35,15 +35,6 @@ module Api
       def user_params
         params.permit(:email, :password, :lastname, :firstname)
       end
-
-      def generate_refresh_token
-        loop do
-          # generate a random token string and return it,
-          # unless there is already another token with the same string
-          token = SecureRandom.hex(32)
-          break token unless Doorkeeper::AccessToken.exists?(refresh_token: token)
-        end
-      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,15 @@ class User < ApplicationRecord
       scopes: ''
     )
   end
+
+  private
+
+  def generate_refresh_token
+    loop do
+      # generate a random token string and return it,
+      # unless there is already another token with the same string
+      token = SecureRandom.hex(32)
+      break token unless Doorkeeper::AccessToken.exists?(refresh_token: token)
+    end
+  end
 end

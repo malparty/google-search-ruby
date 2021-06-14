2c9230608368b5767e28e88b0576fc196ef20f88
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-14 17:11:41 +0700

    [#5] Fix spacing for clarity in authentication helper

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -25,12 +25,15 @@ module DeviseHelpers
 
     def sign_up_ui(email, password, password_confirm = nil)
       user = Fabricate(:user)
+
       visit new_user_registration_path
+
       fill_in 'user_first_name', with: user.first_name
       fill_in 'user_last_name', with: user.last_name
       fill_in 'user_email', with: email
       fill_in 'user_password', with: password
       fill_in 'user_password_confirmation', with: password_confirm || password
+
       click_button 'Sign up'
     end
   end

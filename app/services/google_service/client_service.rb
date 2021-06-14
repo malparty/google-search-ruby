89a9645ce907522fcfbe3ff340dff26bbcc90193
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-14 15:13:54 +0700

    [#6] Replace obsolete URI.encode by CGI.escape

--- app/services/google_service/client_service.rb
+++ app/services/google_service/client_service.rb
@@ -5,8 +5,8 @@ module GoogleService
     require 'httparty'
 
     def self.query(keyword, lang = 'en')
-      encoded_keyword = URI.encode(keyword)
-      uri = URI("https://google.com/search?q=#{encoded_keyword}&hl=#{lang}&gl=#{lang}")
+      escaped_keyword = CGI.escape(keyword)
+      uri = URI("https://google.com/search?q=#{escaped_keyword}&hl=#{lang}&gl=#{lang}")
       user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '\
                 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
       HTTParty.get(uri, { headers: { 'User-Agent' => user_agent } })

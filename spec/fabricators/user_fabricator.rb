76dd4eecd13c98fdf0522326db1667d3d00d9eeb
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-14 17:10:07 +0700

    [#5] Fix missing renaming for lastname firstname

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-require 'ffaker'
-
 Fabricator(:user) do
-  lastname FFaker::Name.last_name
-  firstname FFaker::Name.first_name
+  last_name FFaker::Name.last_name
+  first_name FFaker::Name.first_name
   email FFaker::Internet.email
   password 'password123'
   password_confirmation 'password123'

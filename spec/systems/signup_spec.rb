a52174d16b7ec69c612212edc463d261c7451042
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-14 13:15:27 +0700

    [#5] Fix english mistakes in tests

--- spec/systems/signup_spec.rb
+++ spec/systems/signup_spec.rb
@@ -3,32 +3,32 @@
 require 'rails_helper'
 
 describe 'signup', type: :system do
-  context 'when non logged user reach the sign up page' do
-    it 'displays email field' do
+  context 'when an unauthenticated user reaches the sign up page' do
+    it 'displays the email field' do
       visit new_user_registration_path
       expect(find('form')).to have_field('user_email')
     end
 
-    it 'displays password field ' do
+    it 'displays the password field ' do
       visit new_user_registration_path
       expect(find('form')).to have_field('user_password')
     end
 
-    it 'displays password confirmation field ' do
+    it 'displays the password confirmation field ' do
       visit new_user_registration_path
       expect(find('form')).to have_field('user_password_confirmation')
     end
   end
 
-  context 'when non logged user sign up with good data' do
-    it 'create an account and log the user in' do
+  context 'when an unauthenticated user signs up with good data' do
+    it 'creates an account and logs the user in' do
       sign_up_ui 'good@email.com', 'password123'
 
       expect(page).to have_content(I18n.t('devise.registrations.signed_up'))
     end
   end
 
-  context 'when an non logged user sign up with bad email' do
+  context 'when an unauthenticated user signs up with bad email' do
     it 'stays on the same page' do
       sign_up_ui 'bad_email', 'password123'
 
@@ -36,24 +36,24 @@ describe 'signup', type: :system do
     end
   end
 
-  context 'when an non logged user sign up with too short password' do
-    it 'display an error message' do
+  context 'when an unauthenticated user signs up with a too short password' do
+    it 'displays an error message' do
       sign_up_ui 'good@email.com', '123'
 
       expect(page).to have_selector('#error_explanation li')
     end
   end
 
-  context 'when an non logged user sign up with bad password confirmation' do
-    it 'display an error message' do
+  context 'when an unauthenticated user signs up with bad password confirmation' do
+    it 'displays an error message' do
       sign_up_ui 'good@email.com', 'complex123password', 'differentPassword123'
 
       expect(page).to have_selector('#error_explanation li')
     end
   end
 
-  context 'when a logged user reach the sign up page' do
-    it 'redirect him to the root_page' do
+  context 'when an authenticated user reaches the sign up page' do
+    it 'redirects him to the root_page' do
       sign_in_ui
       visit new_user_registration_path
 

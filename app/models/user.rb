18c5ef4d587bbaa193a4c799f6c73f9d86fdb492
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-14 13:17:49 +0700

    [#5] Remove duplicated email validation rule

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, format: URI::MailTo::EMAIL_REGEXP
-
   # the authenticate method from devise documentation
   def self.authenticate(email, password)
     user = User.find_for_authentication(email: email)

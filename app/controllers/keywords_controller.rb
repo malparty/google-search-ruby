f60ba32cd4dd75c91a866b24bc4f40a9a3b26032
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-16 17:42:07 +0700

    [#6] Handle error while query google - not colorized

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -5,7 +5,9 @@ class KeywordsController < ApplicationController
 
   def create
     keyword = params['keyword']
-    raw_response = GoogleService::ClientService.query(keyword)
+    raw_response = GoogleService::ClientService.new(keyword).query_result
+
+    return redirect_to keywords_path, alert: I18n.t('keywords.could_not_query') unless raw_response
 
     render :create, locals: {
       keyword: keyword,

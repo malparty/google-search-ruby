045ff3072f4d579444b2ad0a782bd6429f635b20
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-16 18:23:51 +0700

    [#6] Colorize logs from Rails.logger

--- app/services/google_service/client_service.rb
+++ app/services/google_service/client_service.rb
@@ -3,6 +3,7 @@
 module GoogleService
   class ClientService
     require 'httparty'
+    require 'colorize'
 
     USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '\
                 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
@@ -26,12 +27,28 @@ module GoogleService
       begin
         @result = HTTParty.get(@uri, { headers: { 'User-Agent' => USER_AGENT } })
       rescue HTTParty::Error, Timeout::Error, SocketError => e
-        Rails.logger.error "Error: Query Google with keyword #{@keyword} throw an error: #{e}"
+        Rails.logger.error "Error: Query Google with keyword #{@keyword} throw an error: #{e}".colorize(:red)
         @result = nil
       else
         validate_result
       end
       @result
     end
+<<<<<<< HEAD
+=======
+
+    private
+
+    # Inspect Http response status code
+    # Any non 200 response code will be logged
+    # response is set to nil in order to notify the error
+    def validate_result
+      return if @result.response.code == '200'
+
+      Rails.logger.warn "Warning: Query Google with keyword #{@keyword} return status code #{@result.response.code}"
+        .colorize(:yellow)
+      @result = nil
+    end
+>>>>>>> d94c7d6 ([#6] Colorize logs from Rails.logger)
   end
 end

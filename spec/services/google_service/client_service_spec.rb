8b3ffc0541b11d778b2cb2900e2e89929d26e27f
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-16 16:28:52 +0700

    [#6] Declare vcr inline to improve code readability

--- spec/services/google_service/client_service_spec.rb
+++ spec/services/google_service/client_service_spec.rb
@@ -4,20 +4,14 @@ require 'rails_helper'
 
 RSpec.describe GoogleService::ClientService, type: :service do
   context 'when querying a simple keyword' do
-    it 'returns an HTTParty Response' do
-      result = nil
-      VCR.use_cassette('google_search') do
-        result = described_class.query(FFaker::Lorem.word)
-      end
+    it 'returns an HTTParty Response', vcr: 'google_search' do
+      result = described_class.query(FFaker::Lorem.word)
 
       expect(result).to be_an_instance_of(HTTParty::Response)
     end
 
-    it 'queries Google Search' do
-      path = nil
-      VCR.use_cassette('google_search') do
-        path = described_class.query(FFaker::Lorem.word).request.path
-      end
+    it 'queries Google Search', vcr: 'google_search' do
+      path = described_class.query(FFaker::Lorem.word).request.path
 
       expect(path.to_s).to start_with('https://www.google.com/search')
     end

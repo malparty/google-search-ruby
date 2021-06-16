11f09cdd18a321df3a647be9d4824b4e0af61100
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-16 16:42:14 +0700

    [#6] Replace instance variables by local variables inside view

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -4,7 +4,12 @@ class KeywordsController < ApplicationController
   def index; end
 
   def create
-    @keyword = params['keyword']
-    @raw_response = GoogleService::ClientService.query(@keyword)
+    keyword = params['keyword']
+    raw_response = GoogleService::ClientService.query(keyword)
+
+    render :create, locals: {
+      keyword: keyword,
+      raw_response: raw_response
+    }
   end
 end

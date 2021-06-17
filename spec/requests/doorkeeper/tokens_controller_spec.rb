d530defffdab41f3df63b25024c6f151660f4250
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-17 15:45:40 +0700

    [#17] Fix test spellings

--- spec/requests/doorkeeper/tokens_controller_spec.rb
+++ spec/requests/doorkeeper/tokens_controller_spec.rb
@@ -3,37 +3,37 @@
 require 'rails_helper'
 
 describe Doorkeeper::TokensController, type: :request do
-  context 'when user ask for a token with good request' do
-    it 'return a refreshable token' do
-      post 'create', params: token_request_params
+  context 'when a user asks for a token with good request' do
+    it 'returns a refreshable token' do
+      post :create, params: token_request_params
       expect(JSON.parse(response.body)['token_type']).to eq('Bearer')
     end
   end
 
-  context 'when user ask for a token without client secret' do
-    it 'return invalid_client error' do
+  context 'when a user asks for a token without client secret' do
+    it 'returns an invalid_client error' do
       params = token_request_params
       params.except!(:client_secret)
 
-      post 'create', params: params
+      post :create, params: params
 
       expect(JSON.parse(response.body)['error']).to eq('invalid_client')
     end
   end
 
-  context 'when user ask for a token with bad credentials' do
-    it 'return invalid_grant error' do
+  context 'when a user asks for a token with bad credentials' do
+    it 'returns an invalid_grant error' do
       params = token_request_params
       params[:password] = 'wrong_pass'
 
-      post 'create', params: params
+      post :create, params: params
 
       expect(JSON.parse(response.body)['error']).to eq('invalid_grant')
     end
   end
 
-  context 'when user refresh an existing token on time' do
-    it 'return the refreshed token' do
+  context 'when a user refreshes an existing token on time' do
+    it 'returns the refreshed token' do
       token = query_token
       refreshed_token = query_refresh_token token['refresh_token']
 
@@ -41,13 +41,13 @@ describe Doorkeeper::TokensController, type: :request do
     end
   end
 
-  context 'when user revoke an existing token' do
-    it 'return no error message ' do
+  context 'when a user revokes an existing token' do
+    it 'returns no error message' do
       token = query_token
       params = token_request_params
       params[:token] = token
 
-      post 'revoke', params: params
+      post :revoke, params: params
 
       expect(JSON.parse(response.body)).to eq({})
     end

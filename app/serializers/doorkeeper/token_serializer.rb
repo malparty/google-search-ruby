620ff08fe4325e670c6d0912788e3ef325ebaf46
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-18 16:49:52 +0700

    [#17] Add line for clarity. remove .keep

--- app/serializers/doorkeeper/token_serializer.rb
+++ app/serializers/doorkeeper/token_serializer.rb
@@ -4,6 +4,7 @@ module Doorkeeper
   class TokenSerializer
     include JSONAPI::Serializer
     attributes :token, :token_type, :expires_in, :refresh_token
+
     attribute :created_at do |token|
       token.created_at.to_time.to_i
     end

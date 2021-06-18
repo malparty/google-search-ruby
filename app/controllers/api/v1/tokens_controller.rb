21f27cb035a40fdd8a15da514f502c3b6ff830f6
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-18 09:20:38 +0700

    [#17] Refactor code and test to be more reusable

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -4,10 +4,7 @@ module API
   module V1
     class TokensController < Doorkeeper::TokensController
       include ErrorHandlerConcern
-
-      after_action :handle_jsonapi_errors, only: %i[revoke create]
-      after_action :handle_jsonapi_revoke, only: :revoke
-      after_action :handle_jsonapi_create, only: :create
+      include JsonAPIHandlerConcern
 
       private
 
@@ -16,26 +13,6 @@ module API
         {
           errors: build_error(detail: error_description, code: :unauthorized_client)
         }
-      end
-
-      def handle_jsonapi_revoke
-        response.body = { meta: I18n.t('doorkeeper.token_revoked') }.to_json if response.status == 200
-      end
-
-      def handle_jsonapi_create
-        return unless response.status == 200
-
-        response.body = { data: { id: 1, type: :token, attributes: JSON.parse(response.body) } }.to_json
-        response.status = 201
-      end
-
-      def handle_jsonapi_errors
-        return if (200..299).cover? response.status
-
-        resp = JSON.parse(response.body)
-        result = { errors: [build_error(detail: resp['error_description'], code: resp['error'])] }
-
-        response.body = result.to_json
       end
     end
   end
--- spec/requests/api/v1/tokens_controller_spec.rb
+++ spec/requests/api/v1/tokens_controller_spec.rb
@@ -2,11 +2,11 @@
 
 require 'rails_helper'
 
-describe Doorkeeper::TokensController, type: :request do
+describe API::V1::TokensController, type: :request do
   context 'when a user asks for a token with good request' do
     it 'returns a refreshable token' do
       post :create, params: token_request_params
-      expect(JSON.parse(response.body)['token_type']).to eq('Bearer')
+      expect(JSON.parse(response.body)['data']['attributes']['token_type']).to eq('Bearer')
     end
   end
 
@@ -16,7 +16,7 @@ describe Doorkeeper::TokensController, type: :request do
 
       post :create, params: params
 
-      expect(JSON.parse(response.body)['error']).to eq('invalid_client')
+      expect(JSON.parse(response.body)['errors'][0]['code']).to eq('invalid_client')
     end
   end
 
@@ -26,7 +26,7 @@ describe Doorkeeper::TokensController, type: :request do
 
       post :create, params: params
 
-      expect(JSON.parse(response.body)['error']).to eq('invalid_grant')
+      expect(JSON.parse(response.body)['errors'][0]['code']).to eq('invalid_grant')
     end
   end
 
@@ -35,18 +35,18 @@ describe Doorkeeper::TokensController, type: :request do
       token = query_token
       refreshed_token = query_refresh_token token['refresh_token']
 
-      expect(refreshed_token['token_type']).to eq('Bearer')
+      expect(refreshed_token['data']['attributes']['token_type']).to eq('Bearer')
     end
   end
 
   context 'when a user revokes an existing token' do
-    it 'returns no error message' do
+    it 'returns a success status code' do
       token = query_token
       params = token_request_params.merge({ token: token })
 
       post :revoke, params: params
 
-      expect(JSON.parse(response.body)).to eq({})
+      expect(response.status).to eq(200)
     end
   end
 end

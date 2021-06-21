c74da8a6fb21107fda2bfa7fa6b2de998f149b57
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-21 11:29:14 +0700

    [#6] Remove create action and index form

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -2,19 +2,4 @@
 
 class KeywordsController < ApplicationController
   def index; end
-
-  def create
-    keyword = params['keyword']
-    raw_response = GoogleService::ClientService.new(keyword).query_result
-
-    if raw_response
-      render :create, locals: {
-        keyword: keyword,
-        raw_response: raw_response
-      }
-      return
-    end
-
-    redirect_to keywords_path, alert: I18n.t('keywords.could_not_query')
-  end
 end

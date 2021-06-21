a61fca61ae3b6b6442780a990deb4d4265b7ee20
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-21 14:21:28 +0700

    [#17] Solve issues after rebase

--- db/migrate/20210614165400_rename_names_for_users.rb
+++
@@ -1,6 +0,0 @@
-class RenameNamesForUsers < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :users, :lastname, :last_name
-    rename_column :users, :firstname, :first_name
-  end
-end

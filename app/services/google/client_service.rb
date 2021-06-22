4f4805cffcd71d1f805250f26f38b8adae33fb94
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-22 15:09:21 +0700

    [#6] Fix nil keyword in error messages

--- app/services/google/client_service.rb
+++ app/services/google/client_service.rb
@@ -8,15 +8,15 @@ module Google
     BASE_SEARCH_URL = 'https://google.com/search'
 
     def initialize(keyword:, lang: 'en')
-      escaped_keyword = CGI.escape(keyword)
-      @uri = URI("#{BASE_SEARCH_URL}?q=#{escaped_keyword}&hl=#{lang}&gl=#{lang}")
+      @escaped_keyword = CGI.escape(keyword)
+      @uri = URI("#{BASE_SEARCH_URL}?q=#{@escaped_keyword}&hl=#{lang}&gl=#{lang}")
     end
 
     def call
       begin
         result = HTTParty.get(@uri, { headers: { 'User-Agent' => USER_AGENT } })
       rescue HTTParty::Error, Timeout::Error, SocketError => e
-        Rails.logger.error "Error: Query Google with keyword #{@keyword} throw an error: #{e}".colorize(:red)
+        Rails.logger.error "Error: Query Google with keyword #{@escaped_keyword} throw an error: #{e}".colorize(:red)
 
         result = nil
       end
@@ -32,7 +32,7 @@ module Google
     def validate_result(result)
       return result if result.response.code == '200'
 
-      Rails.logger.warn "Warning: Query Google with keyword #{@keyword} return status code #{@result.response.code}"
+      Rails.logger.warn "Warning: Query Google with keyword #{@escaped_keyword} return status code #{result.response.code}"
         .colorize(:yellow)
 
       nil

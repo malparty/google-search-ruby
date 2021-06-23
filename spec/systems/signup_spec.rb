9ad10f01ce1fa35a84850a8c7e35d4810b95245f
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-23 10:39:38 +0700

    [#17] Improve signup specs with nested contexts

--- spec/systems/signup_spec.rb
+++ spec/systems/signup_spec.rb
@@ -24,34 +24,36 @@ describe 'signup', type: :system do
   end
 
   context 'when an unauthenticated user signs up with good data' do
-    it 'creates an account and logs the user in' do
-      sign_up_ui 'good@email.com', 'password123'
+    context 'given good params' do
+      it 'creates an account and logs the user in' do
+        sign_up_ui 'good@email.com', 'password123'
 
-      expect(page).to have_content(I18n.t('devise.registrations.signed_up'))
+        expect(page).to have_content(I18n.t('devise.registrations.signed_up'))
+      end
     end
-  end
 
-  context 'when an unauthenticated user signs up with bad email' do
-    it 'stays on the same page' do
-      sign_up_ui 'bad_email', 'password123'
+    context 'given a bad email' do
+      it 'stays on the same page' do
+        sign_up_ui 'bad_email', 'password123'
 
-      expect(page).to have_current_path new_user_registration_path
+        expect(page).to have_current_path new_user_registration_path
+      end
     end
-  end
 
-  context 'when an unauthenticated user signs up with a too short password' do
-    it 'displays an error message' do
-      sign_up_ui 'good@email.com', '123'
+    context 'given a too short password' do
+      it 'displays an error message' do
+        sign_up_ui 'good@email.com', '123'
 
-      expect(page).to have_selector('#error_explanation li')
+        expect(page).to have_selector('#error_explanation li')
+      end
     end
-  end
 
-  context 'when an unauthenticated user signs up with bad password confirmation' do
-    it 'displays an error message' do
-      sign_up_ui 'good@email.com', 'complex123password', 'differentPassword123'
+    context 'given a bad password confirmation' do
+      it 'displays an error message' do
+        sign_up_ui 'good@email.com', 'complex123password', 'differentPassword123'
 
-      expect(page).to have_selector('#error_explanation li')
+        expect(page).to have_selector('#error_explanation li')
+      end
     end
   end
 

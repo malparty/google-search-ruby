777e61b9f021ed0caea2403c3c7a2b807218026e
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-23 17:16:14 +0700

    [#6] Rebase from develop had wrong schema version

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_165400) do
+ActiveRecord::Schema.define(version: 2021_06_21_104559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'citext'
   enable_extension 'plpgsql'

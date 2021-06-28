52348d9bc30092ebc01151d398fb3c73473a25a6
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-28 15:20:37 +0700

    [#26] Use shoulda-matchers test helpers for keyword validation tests

--- spec/models/keyword_spec.rb
+++ spec/models/keyword_spec.rb
@@ -5,20 +5,7 @@ require 'rails_helper'
 RSpec.describe Keyword, type: :model do
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:name) }
-
-    context 'given a blank name' do
-      it 'raises a RecordInvalid error' do
-        expect { Fabricate(:keyword, name: " \n") }.to raise_error(ActiveRecord::RecordInvalid)
-      end
-    end
-
-    context 'given a too long name' do
-      it 'raises a RecordInvalid error' do
-        too_long_name = FFaker::Lorem.characters(256)
-
-        expect { Fabricate(:keyword, name: too_long_name) }.to raise_error(ActiveRecord::RecordInvalid)
-      end
-    end
+    it { is_expected.to validate_length_of(:name).is_at_most(255) }
 
     context 'given a valid name' do
       it 'saves with success' do

7999c1c63f9ea7ba468b3694d8051eba0a3ab4be
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-28 09:39:01 +0700

    [#26] Add keywords method in Keyword_controller to wrap Query Object

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -4,10 +4,17 @@ class KeywordsController < ApplicationController
   include Pagy::Backend
 
   def index
-    pagy, keywords = pagy(KeywordsQuery.new(current_user).call)
+    pagy, keywords = pagy(keywords)
 
     render locals: {
       pagy: pagy, keywords: keywords
     }
   end
+
+  private
+
+  def keywords
+    KeywordsQuery.new(current_user).call
+  end
+
 end

bdd4d8a3cb0d5b11e5e1573cb9945d3b82cb62ec
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-28 09:26:03 +0700

    [#7] Rename vcr to simpler google_search/xx name

--- spec/services/google/client_service_spec.rb
+++ spec/services/google/client_service_spec.rb
@@ -4,13 +4,13 @@ require 'rails_helper'
 
 RSpec.describe Google::ClientService, type: :service do
   context 'when querying a simple keyword' do
-    it 'returns an HTTParty Response', vcr: 'google_search' do
+    it 'returns an HTTParty Response', vcr: 'google_search/base' do
       result = described_class.new(keyword: FFaker::Lorem.word).call
 
       expect(result).to be_an_instance_of(HTTParty::Response)
     end
 
-    it 'queries Google Search', vcr: 'google_search' do
+    it 'queries Google Search', vcr: 'google_search/base' do
       path = described_class.new(keyword: FFaker::Lorem.word).call.request.path
 
       expect(path.to_s).to start_with(described_class::BASE_SEARCH_URL)
@@ -18,13 +18,13 @@ RSpec.describe Google::ClientService, type: :service do
   end
 
   context 'when google returns an HTTP error' do
-    it 'returns false', vcr: 'google_warn' do
+    it 'returns false', vcr: 'google_search/too_many_requests' do
       result = described_class.new(keyword: FFaker::Lorem.word).call
 
       expect(result).to eq(false)
     end
 
-    it 'logs a warning with the escaped keyword', vcr: 'google_warn' do
+    it 'logs a warning with the escaped keyword', vcr: 'google_search/too_many_requests' do
       allow(Rails.logger).to receive(:warn)
 
       word = FFaker::Lorem.word
--- spec/services/google/parser_service_spec.rb
+++ spec/services/google/parser_service_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Google::ParserService, type: :service do
   context 'when parsing a page having 1 top ad' do
-    it 'counts exactly 1 top ad', vcr: 'google_search_top_ads_1' do
+    it 'counts exactly 1 top ad', vcr: 'google_search/top_ads_1' do
       result = Google::ClientService.new(keyword: 'squarespace').call
 
       expect(described_class.new(html_response: result).call[:ads_top_count]).to eq(1)
@@ -12,37 +12,37 @@ RSpec.describe Google::ParserService, type: :service do
   end
 
   context 'when parsing a page having 3 top ads, 3 bottom ads and 14 non ad links' do
-    it 'counts exactly 3 top ads', vcr: 'google_search_top_ads_6' do
+    it 'counts exactly 3 top ads', vcr: 'google_search/top_ads_6' do
       result = Google::ClientService.new(keyword: 'vpn').call
 
       expect(described_class.new(html_response: result).call[:ads_top_count]).to eq(3)
     end
 
-    it 'counts exactly 6 ads in total', vcr: 'google_search_top_ads_6' do
+    it 'counts exactly 6 ads in total', vcr: 'google_search/top_ads_6' do
       result = Google::ClientService.new(keyword: 'vpn').call
 
       expect(described_class.new(html_response: result).call[:ads_page_count]).to eq(6)
     end
 
-    it 'finds exactly the 3 top ads urls', vcr: 'google_search_top_ads_6' do
+    it 'finds exactly the 3 top ads urls', vcr: 'google_search/top_ads_6' do
       result = Google::ClientService.new(keyword: 'vpn').call
 
       expect(described_class.new(html_response: result).call[:ads_top_url]).to contain_exactly('https://cloud.google.com/free', 'https://www.expressvpn.com/', 'https://www.top10vpn.com/best-vpn-for-vietnam/')
     end
 
-    it 'counts exactly 14 non ad results', vcr: 'google_search_top_ads_6' do
+    it 'counts exactly 14 non ad results', vcr: 'google_search/top_ads_6' do
       result = Google::ClientService.new(keyword: 'vpn').call
 
       expect(described_class.new(html_response: result).call[:non_ads_result_count]).to eq(14)
     end
 
-    it 'gets 14 results', vcr: 'google_search_top_ads_6' do
+    it 'gets 14 results', vcr: 'google_search/top_ads_6' do
       result = Google::ClientService.new(keyword: 'vpn').call
 
       expect(described_class.new(html_response: result).call[:non_ads_url].count).to eq(14)
     end
 
-    it 'gets exactly 113 links', vcr: 'google_search_top_ads_6' do
+    it 'gets exactly 113 links', vcr: 'google_search/top_ads_6' do
       # Counted from cassette html raw code
       result = Google::ClientService.new(keyword: 'vpn').call
 

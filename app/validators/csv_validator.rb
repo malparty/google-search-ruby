b674a010e234b805f6f42285a7df734ba6596ee0
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-29 20:06:00 +0700

    non-working [#9] Bulk keyword saving implemented

--- app/validators/csv_validator.rb
+++ app/validators/csv_validator.rb
@@ -2,19 +2,19 @@
 
 class CSVValidator < ActiveModel::Validator
   def validate(record)
-    record.errors.add(attribute, I18n.t('csv.validation.blank')) if is_blank?
-    record.errors.add(attribute, I18n.t('csv.validation.too_many_keywords')) if has_too_many_keywords?
-    record.errors.add(attribute, I18n.t('csv.validation.readable')) unless is_readable?
+    record.errors.add(:file, I18n.t('csv.validation.blank')) if is_blank?
+    record.errors.add(:file, I18n.t('csv.validation.too_many_keywords')) if has_too_many_keywords?
+    record.errors.add(:file, I18n.t('csv.validation.readable')) unless is_readable?
   end
 
   private
 
   def is_blank?
-    true
+    false
   end
 
   def has_too_many_keywords?
-    true
+    false
   end
 
   def is_readable?

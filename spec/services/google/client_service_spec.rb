41d05a6bf0f66311b627b0ec3bba8c777223c71f
Xavier MALPARTY <xavier@nimblehq.co>
2021-06-30 18:41:59 +0700

    [#7] Wrap specs with describe #method_name

--- spec/services/google/client_service_spec.rb
+++ spec/services/google/client_service_spec.rb
@@ -3,34 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe Google::ClientService, type: :service do
-  context 'when querying a simple keyword' do
-    it 'returns an HTTParty Response', vcr: 'google_search/base' do
-      result = described_class.new(keyword: FFaker::Lorem.word).call
+  describe '#call' do
+    context 'when querying a simple keyword' do
+      it 'returns an HTTParty Response', vcr: 'google_search/base' do
+        result = described_class.new(keyword: FFaker::Lorem.word).call
 
-      expect(result).to be_an_instance_of(HTTParty::Response)
+        expect(result).to be_an_instance_of(HTTParty::Response)
+      end
+
+      it 'queries Google Search', vcr: 'google_search/base' do
+        path = described_class.new(keyword: FFaker::Lorem.word).call.request.path
+
+        expect(path.to_s).to start_with(described_class::BASE_SEARCH_URL)
+      end
     end
 
-    it 'queries Google Search', vcr: 'google_search/base' do
-      path = described_class.new(keyword: FFaker::Lorem.word).call.request.path
+    context 'when google returns an HTTP error' do
+      it 'returns false', vcr: 'google_search/too_many_requests' do
+        result = described_class.new(keyword: FFaker::Lorem.word).call
 
-      expect(path.to_s).to start_with(described_class::BASE_SEARCH_URL)
-    end
-  end
+        expect(result).to eq(false)
+      end
 
-  context 'when google returns an HTTP error' do
-    it 'returns false', vcr: 'google_search/too_many_requests' do
-      result = described_class.new(keyword: FFaker::Lorem.word).call
+      it 'logs a warning with the escaped keyword', vcr: 'google_search/too_many_requests' do
+        allow(Rails.logger).to receive(:warn)
 
-      expect(result).to eq(false)
-    end
+        word = FFaker::Lorem.word
+        described_class.new(keyword: word).call
 
-    it 'logs a warning with the escaped keyword', vcr: 'google_search/too_many_requests' do
-      allow(Rails.logger).to receive(:warn)
-
-      word = FFaker::Lorem.word
-      described_class.new(keyword: word).call
-
-      expect(Rails.logger).to have_received(:warn).with(/#{CGI.escape(word)}/)
+        expect(Rails.logger).to have_received(:warn).with(/#{CGI.escape(word)}/)
+      end
     end
   end
 end

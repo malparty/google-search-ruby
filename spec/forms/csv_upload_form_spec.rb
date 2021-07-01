134ecd78ba88f69977a59ba13543f5bab3deb7b7
Xavier MALPARTY <xavier@nimblehq.co>
2021-07-01 10:10:34 +0700

    [#9] Wrap form spec with describe #save

--- spec/forms/csv_upload_form_spec.rb
+++ spec/forms/csv_upload_form_spec.rb
@@ -3,51 +3,53 @@
 require 'rails_helper'
 
 RSpec.describe CSVUploadForm, type: :form do
-  context 'given a valid file of 8 keywords' do
-    it { expect { save_csv_file 'valid.csv' }.to change(Keyword, :count).by(8) }
+  describe '#save' do
+    context 'given a valid file of 8 keywords' do
+      it { expect { save_csv_file 'valid.csv' }.to change(Keyword, :count).by(8) }
 
-    it 'can save' do
-      _, _, saved = save_csv_file 'valid.csv'
+      it 'can save' do
+        _, _, saved = save_csv_file 'valid.csv'
 
-      expect(saved).to be(true)
+        expect(saved).to be(true)
+      end
+
+      it 'parses the keyword containing comma symbol' do
+        form, = save_csv_file 'valid.csv'
+
+        expect(form.keywords.map(&:name)).to include('dien may xanh, the gio di dong')
+      end
     end
 
-    it 'parses the keyword containing comma symbol' do
-      form, = save_csv_file 'valid.csv'
+    context 'given too many keywords' do
+      it 'returns a wrong_count error' do
+        form, = save_csv_file 'too_many_keywords.csv'
 
-      expect(form.keywords.map(&:name)).to include('dien may xanh, the gio di dong')
+        expect(form.errors.full_messages).to include(I18n.t('csv.validation.wrong_count'))
+      end
     end
-  end
 
-  context 'given too many keywords' do
-    it 'returns a wrong_count error' do
-      form, = save_csv_file 'too_many_keywords.csv'
+    context 'given a blank csv file' do
+      it 'returns a blank error' do
+        form, = save_csv_file 'blank.csv'
 
-      expect(form.errors.full_messages).to include(I18n.t('csv.validation.wrong_count'))
+        expect(form.errors.full_messages).to include(I18n.t('csv.validation.blank'))
+      end
     end
-  end
 
-  context 'given a blank csv file' do
-    it 'returns a blank error' do
-      form, = save_csv_file 'blank.csv'
+    context 'given a non csv file' do
+      it 'returns a wrong_type error' do
+        form, = save_csv_file 'wrong_type.txt'
 
-      expect(form.errors.full_messages).to include(I18n.t('csv.validation.blank'))
+        expect(form.errors.full_messages).to include(I18n.t('csv.validation.wrong_type'))
+      end
     end
-  end
 
-  context 'given a non csv file' do
-    it 'returns a wrong_type error' do
-      form, = save_csv_file 'wrong_type.txt'
+    context 'given 2 invalid keywords' do
+      it 'returns 2 keyword errors' do
+        form, = save_csv_file 'invalid_keywords.csv'
 
-      expect(form.errors.full_messages).to include(I18n.t('csv.validation.wrong_type'))
-    end
-  end
-
-  context 'given 2 invalid keywords' do
-    it 'returns 2 keyword errors' do
-      form, = save_csv_file 'invalid_keywords.csv'
-
-      expect(form.errors.errors.count { |e| e.attribute == :keyword }).to eq(2)
+        expect(form.errors.errors.count { |e| e.attribute == :keyword }).to eq(2)
+      end
     end
   end
 end

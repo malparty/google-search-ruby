1825da40c136d84696a73b7edf397ce1e89ec4d5
Xavier MALPARTY <xavier@nimblehq.co>
2021-07-01 10:15:07 +0700

    [#9] Change describe class by explicit class name in helper

--- spec/support/file_upload_helpers.rb
+++ spec/support/file_upload_helpers.rb
@@ -4,7 +4,7 @@ module FileUploadHelpers
   def save_csv_file(file_name)
     file = file_fixture("csv/#{file_name}")
 
-    form = described_class.new(Fabricate(:user))
+    form = CSVUploadForm.new(Fabricate(:user))
 
     saved = form.save({ file: file })
 

5a92484a945ca8a5c695e7b507b9af1c0b204ece
Xavier MALPARTY <xavier@nimblehq.co>
2021-07-02 15:13:24 +0700

    [#9] Add db level constraint. Remove model level bulck validation.

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -4,17 +4,8 @@ class Keyword < ApplicationRecord
   include Discard::Model
 
   validates :name, presence: true, length: { maximum: 255 }
-  validates :user, presence: true, unless: :bulk_insert?
 
-  # optional belongs_to in order to skip FK validation on bulk_insert
-  belongs_to :user, inverse_of: :keywords, optional: true
+  belongs_to :user, inverse_of: :keywords
 
   default_scope -> { kept }
-
-  attr_accessor :bulk_insert
-
-  # disable validations generating SQL queries for bulk insert
-  def bulk_insert?
-    bulk_insert
-  end
 end
--- app/validators/csv_validator.rb
+++ app/validators/csv_validator.rb
@@ -7,7 +7,6 @@ class CSVValidator < ActiveModel::Validator
     @csv_form = csv_form
 
     validate_file
-    validate_keywords if csv_form.errors.empty?
   end
 
   private
@@ -18,19 +17,9 @@ class CSVValidator < ActiveModel::Validator
     csv_form.file
   end
 
-  def keywords
-    csv_form.keywords
-  end
-
   def validate_file
     add_error :wrong_count unless valid_count?
     add_error :wrong_type unless valid_extension?
-  end
-
-  def validate_keywords
-    keywords_errors = keywords.each(&:validate).map { |keyword| keyword.errors.errors }.flatten.uniq
-
-    keywords_errors.each { |error| csv_form.errors.add(:keyword, error.message) }
   end
 
   def add_error(type)

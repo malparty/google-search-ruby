e1e427dc03aef23d979d40c62581c57bade4ae5e
Xavier MALPARTY <xavier@nimblehq.co>
2021-07-05 15:19:32 +0700

    [#8] Remove duplicated presence validation of belongs_to keyword

--- app/models/result_link.rb
+++ app/models/result_link.rb
@@ -6,5 +6,4 @@ class ResultLink < ApplicationRecord
   enum link_type: { ads_top: 0, ads_page: 1, non_ads: 2 }
 
   validates :url, presence: true, allow_blank: false
-  validates :keyword, presence: true
 end

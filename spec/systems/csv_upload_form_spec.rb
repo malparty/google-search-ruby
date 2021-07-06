d8bf5268faa8dc877b11af05421f656082dbb057
Xavier MALPARTY <xavier@nimblehq.co>
2021-07-06 15:37:04 +0700

    [#13] Improve authentication sign_in helper for system tests

--- spec/systems/csv_upload_form_spec.rb
+++ spec/systems/csv_upload_form_spec.rb
@@ -5,9 +5,7 @@ require 'rails_helper'
 describe 'csv upload form', type: :system do
   describe 'keywords#index' do
     context 'when user reach the home page' do
-      it 'displays the file upload form' do
-        sign_in Fabricate(:user)
-
+      it 'displays the file upload form', authenticated_user: true do
         visit root_path
 
         expect(find('form.form-csv-upload')).to have_field('csv_upload_form_file', visible: :all)
@@ -17,13 +15,13 @@ describe 'csv upload form', type: :system do
 
   describe 'keywords#create' do
     context 'given a valid csv file' do
-      it 'redirects to the homepage' do
+      it 'redirects to the homepage', authenticated_user: true  do
         submit_file 'valid.csv'
 
         expect(page).to have_current_path(keywords_path)
       end
 
-      it 'displays an upload success message' do
+      it 'displays an upload success message', authenticated_user: true  do
         submit_file 'valid.csv'
 
         expect(find('.alert.alert-success')).to have_content(I18n.t('csv.upload_success'))
@@ -31,7 +29,7 @@ describe 'csv upload form', type: :system do
     end
 
     context 'given a text file' do
-      it 'displays the wrong_type error' do
+      it 'displays the wrong_type error', authenticated_user: true  do
         submit_file 'wrong_type.txt'
 
         expect(find('.alert.alert-danger')).to have_content(I18n.t('csv.validation.wrong_type'))
@@ -39,7 +37,7 @@ describe 'csv upload form', type: :system do
     end
 
     context 'given a file with too many keywords' do
-      it 'displays the wrong_count error' do
+      it 'displays the wrong_count error', authenticated_user: true  do
         submit_file 'too_many_keywords.csv'
 
         expect(find('.alert.alert-danger')).to have_content(I18n.t('csv.validation.wrong_count'))
@@ -47,7 +45,7 @@ describe 'csv upload form', type: :system do
     end
 
     context 'given a file with a too long keyword' do
-      it 'displays the bad_keyword_length error' do
+      it 'displays the bad_keyword_length error', authenticated_user: true  do
         submit_file 'invalid_keywords.csv'
 
         expect(find('.alert.alert-danger')).to have_content(I18n.t('csv.validation.bad_keyword_length'))

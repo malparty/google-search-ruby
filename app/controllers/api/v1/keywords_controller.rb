5e8bf7f87ae740e9ee2bd797fbc57658175ac214
Xavier MALPARTY <xavier@nimblehq.co>
2021-07-07 13:39:33 +0700

    [#19] Add api/v1/keyword POST to upload csv file

--- app/controllers/api/v1/keywords_controller.rb
+++ app/controllers/api/v1/keywords_controller.rb
@@ -12,10 +12,32 @@ module API
         render json: KeywordSerializer.new(keywords_list, pagy_options(pagy))
       end
 
+      def create
+        if csv_form.save(create_params[:file])
+          render json: success_create
+        else
+          render json: build_error(detail: csv_form.errors.full_messages, code: :invalid_file)
+        end
+      end
+
       private
 
       def keywords
         KeywordsQuery.new(current_user).call
+      end
+
+      def csv_form
+        @csv_form ||= CSVUploadForm.new(current_user)
+      end
+
+      def create_params
+        params.permit(:file)
+      end
+
+      def success_create
+        {
+          meta: I18n.t('csv.upload_success')
+        }
       end
     end
   end

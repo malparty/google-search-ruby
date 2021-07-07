ca856b5225b0e275f98e86ff885af11227ef3273
Xavier MALPARTY <xavier@nimblehq.co>
2021-07-07 11:28:30 +0700

    [#57] Add filter canvas for mobile and desktop. Add xhr JavaScript helper

--- app/javascript/vendor/bootstrap/index.js
+++ app/javascript/vendor/bootstrap/index.js
@@ -1,13 +1,14 @@
 import bootstrap from 'bootstrap.native/dist/bootstrap-native';
 
-export const Alert = bootstrap.Alert;
-export const Button = bootstrap.Button;
-export const Carousel = bootstrap.Carousel;
-export const Collapse = bootstrap.Collapse;
-export const Dropdown = bootstrap.Dropdown;
-export const Modal = bootstrap.Modal;
-export const Popover = bootstrap.Popover;
-export const ScrollSpy = bootstrap.ScrollSpy;
-export const Tab = bootstrap.Tab;
-export const Toast = bootstrap.Toast;
-export const Tooltip = bootstrap.Tooltip;
+export const Offcanvas = bootstrap.Offcanvas;
+// export const Alert = bootstrap.Alert;
+// export const Button = bootstrap.Button;
+// export const Carousel = bootstrap.Carousel;
+// export const Collapse = bootstrap.Collapse;
+// export const Dropdown = bootstrap.Dropdown;
+// export const Modal = bootstrap.Modal;
+// export const Popover = bootstrap.Popover;
+// export const ScrollSpy = bootstrap.ScrollSpy;
+// export const Tab = bootstrap.Tab;
+// export const Toast = bootstrap.Toast;
+// export const Tooltip = bootstrap.Tooltip;

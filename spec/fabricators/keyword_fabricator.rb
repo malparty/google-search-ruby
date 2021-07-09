097f1c663ce9c1b819685d243f70c9fb42481afc
Xavier MALPARTY <xavier@nimblehq.co>
2021-07-09 11:43:32 +0700

    .#18 Add seeds for keyword_with_result

--- spec/fabricators/keyword_fabricator.rb
+++ spec/fabricators/keyword_fabricator.rb
@@ -4,3 +4,17 @@ Fabricator(:keyword) do
   name { FFaker::FreedomIpsum.word }
   user { Fabricate(:user) }
 end
+
+Fabricator(:keyword_with_result, from: :keyword) do
+  name { FFaker::FreedomIpsum.word }
+  user { Fabricate(:user) }
+
+  status :parsed
+
+  ads_top_count { FFaker.rand 6 }
+  ads_page_count { |attrs| attrs[:ads_top_count] + FFaker.rand(3) }
+  non_ads_result_count { |attrs| 15 + attrs[:ads_top_count] }
+  total_link_count { |attrs| FFaker.rand(30) + attrs[:non_ads_result_count] }
+
+  html { FFaker::HTMLIpsum.body }
+end

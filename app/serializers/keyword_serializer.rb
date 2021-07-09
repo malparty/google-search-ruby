0d37218c2a084d49b1014e6e13de4bce4fde39ea
Xavier MALPARTY <xavier@nimblehq.co>
2021-07-09 11:57:50 +0700

    .#18 Add basic results to api/v1/keywords

--- app/serializers/keyword_serializer.rb
+++ app/serializers/keyword_serializer.rb
@@ -3,5 +3,5 @@
 class KeywordSerializer
   include JSONAPI::Serializer
 
-  attributes :name, :created_at
+  attributes :name, :created_at, :status, :ads_top_count, :ads_page_count, :non_ads_result_count, :total_link_count
 end

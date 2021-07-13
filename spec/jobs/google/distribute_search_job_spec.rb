e01f39bc5dd1fcc4b0490238902eabea5ab2d89d
Xavier MALPARTY <xavier@nimblehq.co>
2021-07-13 13:34:20 +0700

    .#38 Add Google::DistributeSearchJob tests

--- spec/jobs/google/distribute_search_job_spec.rb
+++ spec/jobs/google/distribute_search_job_spec.rb
@@ -3,5 +3,31 @@
 require 'rails_helper'
 
 RSpec.describe Google::DistributeSearchJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  describe '#perform' do
+    context 'given 3 valid keywords' do
+      it 'queues a DistributedSearch job' do
+        keywords = Fabricate.times(3, :keyword)
+
+        expect { described_class.perform_later keywords }.to have_enqueued_job(described_class)
+      end
+
+      it 'queues 3 SearchKeyword jobs' do
+        keywords = Fabricate.times(3, :keyword)
+
+        expect { described_class.perform_now keywords }.to have_enqueued_job(Google::SearchKeywordJob).exactly(:thrice)
+      end
+    end
+
+    context 'given an empty array' do
+      it 'queues a DistributedSearch job' do
+        expect { described_class.perform_later [] }.to have_enqueued_job(described_class)
+      end
+
+      it 'does not queue the SearchKeyword jobs' do
+        expect { described_class.perform_now [] }.not_to have_enqueued_job(Google::SearchKeywordJob)
+      end
+    end
+  end
 end
